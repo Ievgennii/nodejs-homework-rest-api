da0854dc3e89bcdb9ea60a192d64f4aa4172eac8
Ievgennii <simonovievgenii@gmail.com>
2023-07-13 00:50:37 +0300

    add helpers

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,13 +4,41 @@ const router = express.Router()
 
 const contacts = require('../../models/contacts')
 
+const {HttpError} = require("../../helpers")
+
 router.get('/', async (req, res, next) => {
-  const result = await contacts.listContacts()
-  res.json(result)
+  try {
+    const result = await contacts.listContacts()
+    res.json(result)
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error"
+    })
+  }
+
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  // res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params
+    const result = await contacts.getContactById(contactId)
+    if (!result) {
+      throw HttpError (404, "Not found")
+      // const error = new Error("Not found")
+      // error.status = 404
+      // throw error
+      // return res.status(404).json({
+      //   message: "Not found"
+      // })
+    }
+    res.json(result)
+  } catch (error) {
+    const { status = 500, message = "Server error" } = error
+    res.status(status).json({
+      message,
+    })
+  }
 })
 
 router.post('/', async (req, res, next) => {
